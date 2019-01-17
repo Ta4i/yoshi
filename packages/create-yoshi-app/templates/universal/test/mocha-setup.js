9afaf932db5e17ad8ccade93eebd1b71cee98dde
Maksym Kramarenko <maksym@wix.com>
2019-01-17 17:36:09 +0200

    universal + universal-typescript + fullstack

--- packages/create-yoshi-app/templates/universal/test/mocha-setup.js
+++ packages/create-yoshi-app/templates/universal/test/mocha-setup.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { wixAxiosConfig } from '@wix/wix-axios-config';
 import { baseURL } from './test-common';
+import * as Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 wixAxiosConfig(axios, { baseURL });
