e017829b57fafa12347dad1f188e2fe0c808af24
Maksym Kramarenko <maksym@wix.com>
2019-01-17 19:02:31 +0200

    Fix typing in templates/component-library-typescript

--- packages/create-yoshi-app/templates/component-library-typescript/src/Hello/Hello.tsx
+++ packages/create-yoshi-app/templates/component-library-typescript/src/Hello/Hello.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
 import * as s from './Hello.scss';
 
-const Hello: React.StatelessComponent<{ name: string }> = ({ name }) => (
+const Hello: React.FunctionComponent<{ name: string }> = ({ name }) => (
   <div>
     Hello <span className={s.underline}>{name}!</span>
   </div>
 );
-
-Hello.propTypes = {
-  name: PropTypes.string.isRequired,
-};
 
 export default Hello;
